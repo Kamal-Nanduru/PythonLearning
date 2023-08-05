e2241504c4b952e25a7c50650996504e1a128f17
Kamal-Nanduru <114358522+Kamal-Nanduru@users.noreply.github.com>
2023-08-05 13:28:03 +0530

    first python commit

---
+++ class1.py
@@ -0,0 +1,23 @@
+# print ("Hello World")
+
+kiran = "goodboy"
+
+print (id(kiran))
+
+# kiran = "kamal"
+
+# print (id(kiran))
+
+a = {1, "kamal", "arjun", 85, 56}
+a.add("kiran")
+a.remove("kamal")
+print(a)
+
+# a [2] = "RAM"
+# print(a)
+
+# a.insert(4, "Bheem")
+# print(a)
+
+# a.append("Rishi")
+# print(a)
